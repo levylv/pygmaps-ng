1a736ae0559b56b9185778eaf336209e89b200c6
Elliot <elbiot@elbitop>
2014-06-18 16:54:07 -0500

    pip install seems to work

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='pygmaps_ng',
+    version='1.0dev',
+    author='Elliot Hallmark',
+    url='https://github.com/Permafacture/pygmaps-ng/',
+    packages=['pygmaps_ng'])
