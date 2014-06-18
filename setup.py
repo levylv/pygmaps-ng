486c81de4e1917b9e8ff73d5ca01c7edda2599ca
Elliot <elbiot@elbitop>
2014-06-18 11:40:58 -0500

    Moved examples out.  trying to make setuptools compatible

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='pygmaps-ng',
+    version='1.0',
+    author='Elliot Hallmark',
+    url='https://github.com/Permafacture/pygmaps-ng',
+    packages=['pygmaps_ng'])
