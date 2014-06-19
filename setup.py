44a23d13c1959cfe84a340ddadb23edc6d40add7
Elliot <elbiot@elbitop>
2014-06-19 15:20:58 -0500

    Setup.py now tracking media files

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(name='pygmaps_ng',
     version='1.0dev',
     author='Elliot Hallmark',
     url='https://github.com/Permafacture/pygmaps-ng/',
-    packages=['pygmaps_ng'])
+    packages=['pygmaps_ng'],
+    data_files=[('gmm-up',
+                 ['pygmaps_ng/gmm-up/static.html',
+                  'pygmaps_ng/gmm-up/javascripts/main.js',
+                  'pygmaps_ng/gmm-up/stylesheets/main.css'])]
+    )
