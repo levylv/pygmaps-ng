d6d31bb29e0fdee2e4bfaab28e4e3353d0b0ba79
Jason Short <jason@sheersky.com>
2014-06-29 20:31:06 -0700

    Removed jsmin file and added the jsmin package to requirements in setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ name = 'pygmaps_ng'
 datadir = path.join(name,'gmmup')
 packagefiles = [path.join('gmmup',path.basename(f)) for f in glob(path.join(datadir,'*'))]
 
-print {'%s.gmmup'%name:packagefiles}
+print({'%s.gmmup'%name:packagefiles})
 setup(name=name,
     version='1.0dev',
     author='Elliot Hallmark',
