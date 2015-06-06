edf521a457545aca37f3f1fc85fa1ffcf423c353
Elliot <elbiot@elbitop>
2015-06-06 14:12:47 -0500

    example usage added to help(pygmaps_ng)

--- pygmaps_ng/__init__.py
+++ pygmaps_ng/__init__.py
@@ -1,3 +1,28 @@
+'''
+Create stand alone google maps data visualizations.
+
+Data is grouped into datasets of points, lines and complex polygons, 
+which are grouped into Apps. Visibility of data sets and groups of 
+data sets (apps) can be toggled dynamically in browser.
+
+Usage:
+
+from pygmaps_ng import *
+
+mymap = Map()
+app1 = App('test1',title="Test #1")
+mymap.apps.append(app1)
+
+dataset1 = DataSet('data1', title="Point test" ,key_color='FF0088')
+app1.datasets.append(dataset1)
+
+pt = [40.7,-74.0]
+dataset1.add_marker(pt ,title="click me",color="000000",
+  text="<a href='http://en.wikipedia.org/wiki/New_York'>New York!</a>")
+
+mymap.build_page(center=pt,zoom=14,outfile="NYC.html")
+'''
+
 from pygmaps_ng import *
 import matplottools
 import color_gen
