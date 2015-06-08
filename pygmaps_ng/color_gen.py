9d074c4bdefa3ce21f96eaf2718d428e9ffefeda
Elliot <elbiot@elbitop>
2015-06-07 19:42:55 -0500

    added linear interpolation of colors

--- pygmaps_ng/color_gen.py
+++ pygmaps_ng/color_gen.py
@@ -43,4 +43,28 @@ def gradient(n,start=(0,0,255),end=(255,255,255),overflow=(0,0,0)):
           i=0
     while True:
       yield "#%02X%02X%02X" % overflow
-    
+   
+class LinearInterp(object):
+    '''For returning colors between a max and min for input values
+    between max and min'''
+
+    def __init__(self,minval,maxval,start=(255,255,255),end=(0,0,0)):
+        '''takes a minimum value (float or int) and a maximum, for 
+        returning colors graded between start and stop'''
+        self.minval = minval
+        self.maxval = maxval
+        self.start = start
+        self.diff = [e-s for s,e in zip(start,end)]
+
+    def interp(self,val,bug_color=(255,0,0)):
+        '''Takes a value between min and max 
+        Returns a hex color for use in a map between start and end
+        If val is out of range, return the bug color
+        '''
+        start = self.start
+        diff = self.diff
+        p = (val - self.minval)/(float(self.maxval)-self.minval)
+        if (0 <= p <= 1):
+          return '#%02X%02X%02X' % tuple((s+d*p for s,d in zip(start,diff)))
+        else:
+          return '#%02X%02X%02X' % bug_color
